8f7fe3b8841ff78d8808620b75f2c268814e5ce1
Kathy <i375722@fhict.nl>
2022-10-17 00:11:19 +0200

    Initial commit

---
+++ models/comment.js
@@ -0,0 +1,13 @@
+function Comment(content, timestamp, videotimeline) {
+    this.content = content;
+    this.timestamp = timestamp;
+    this.videotimeline = videotimeline;
+    // TODO
+    // likes/dislikes
+    // replies
+
+    // Functions/Methods
+    // this.changeName = function (name) {
+    //     this.lastName = name;
+    // };
+}
